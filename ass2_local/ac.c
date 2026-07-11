fcd5105f066d2479e64fe053c57c72f571103950
77anuj77 <anujparoha07@gmail.com>
2026-07-11 22:40:31 +0530

    eve and odd

---
+++ ass2_local/ac.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter a num:");
+    scanf("%d",&num);
+
+    if(num%2==0){
+        printf("Even num");
+    }
+    else{
+        printf("Odd num");
+    }
+    return 0;
+}
